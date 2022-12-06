72a10e8b1c9b8e293d6a800c41ce5561c19d2754
U_M0ZUN <alexandr0751@yandex.ru>
2022-12-06 21:59:18 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Main {
+    private static void saveGame(String path, GameProgress gp) {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(path))) {
+            outputStream.writeObject(gp);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void zipAndDeleteFiles(String path, List<String> listOfObjects) {
+        FileInputStream fis = null;
+        try (ZipOutputStream zipInputStream = new ZipOutputStream(new FileOutputStream(path))) {
+            for (String a: listOfObjects) {
+                fis = new FileInputStream(a);
+                ZipEntry entry = new ZipEntry(a.substring(a.lastIndexOf("/") + 1));
+                zipInputStream.putNextEntry(entry);
+                zipInputStream.write(fis.readAllBytes());
+                zipInputStream.closeEntry();
+                try {
+                    if (fis!=null) {
+                        fis.close();
+                    }
+                }
+                catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        for (String a: listOfObjects) {
+            File gp = new File(a);
+            gp.delete();
+        }
+    }
+
+    public static void main(String[] args) {
+        saveGame("Games//savegames//gp1.dat",
+                new GameProgress(100, 2, 5, 100));
+        saveGame("Games//savegames//gp2.dat",
+                new GameProgress(80, 3, 50, 20));
+        saveGame("Games//savegames//gp3.dat",
+                new GameProgress(99, 99, 1, 1));
+
+        zipAndDeleteFiles("Games//savegames//zip.zip",new ArrayList<>(Arrays.asList(
+                "Games//savegames//gp1.dat",
+                "Games//savegames//gp2.dat",
+                "Games//savegames//gp3.dat"
+        )));
+    }
+}
